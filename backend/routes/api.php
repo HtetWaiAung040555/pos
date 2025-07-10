8049e52c0a11c912258e334b97f35f382aeae642
BomeMyatKyaw <bomekyaw13@gmail.com>
2025-07-10 11:52:52 +0630

    Changes to be committed: 	modified:   backend/app/Http/Controllers/Api/UsersController.php 	modified:   backend/routes/api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,12 +8,16 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    // Route::get('/users', [UsersController::class, 'index']);
-    Route::get('/users/{id}', [UsersController::class, 'show']);
-    Route::post('/users', [UsersController::class, 'store']);
-    Route::put('/users/{id}', [UsersController::class, 'update']);
-    Route::delete('/users/{id}', [UsersController::class, 'destroy']);
-});
+// Route::middleware(['auth:sanctum'])->group(function () {
+//     Route::get('/users', [UsersController::class, 'index']);
+//     Route::get('/users/{id}', [UsersController::class, 'show']);
+//     Route::post('/user', [UsersController::class, 'store']);
+//     Route::put('/users/{id}', [UsersController::class, 'update']);
+//     Route::delete('/users/{id}', [UsersController::class, 'destroy']);
+// });
 
 Route::get('/users', [UsersController::class, 'index']);
+Route::get('/user/{id}', [UsersController::class, 'show']);
+Route::post('/user', [UsersController::class, 'store']);
+Route::put('/user/{id}', [UsersController::class, 'update']);
+Route::delete('/user/{id}', [UsersController::class, 'destroy']);
