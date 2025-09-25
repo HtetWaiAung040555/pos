fd7c5513208d841ce4f706c9a56c17498356ab20
BomeMyatKyaw <bomekyaw13@gmail.com>
2025-09-25 16:27:57 +0630

    250920251527

--- backend/app/Http/Controllers/Api/WarehousesController.php
+++ backend/app/Http/Controllers/Api/WarehousesController.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\WarehouseResource;
 use App\Models\Warehouse;
-use Illuminate\Http\Request;
 
 class WarehousesController extends Controller
 {
     public function index()
     {
-        $branches = Warehouse::with(['warehouse','status','createdBy', 'updatedBy'])->get();
-        return WarehouseResource::collection($branches);
+        $warehouses = Warehouse::with(['createdBy', 'updatedBy'])->get();
+        return WarehouseResource::collection($warehouses);
     }
 }
